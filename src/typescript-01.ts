5402e8d3e3f99cf355c99fa63f23a978e31673bb
Denny Ribeiro <denny.ribeiro@outlook.com>
2022-06-12 22:29:43 -0300

    working

--- src/typescript-01.ts
+++ src/typescript-01.ts
@@ -2,94 +2,114 @@ import inquirer from "inquirer";
 import { Movie } from "./interfaces/interface.Movie";
 import { User } from "./interfaces/interface.User";
 import { MovieService } from "./service/service.MovieService";
+import { addAverageToMoviesArray } from "./utils/util.addAverageToMoviesArray";
 
 //Generics
 
-const user: User[] = [{
+const users: User[] = [
+  {
     id: 1,
     name: "Denny",
-    age: 9
-}]
+    age: 9,
+  },
+];
 
-let movies: Movie[]
+let movies: Movie[];
 
-const request = new MovieService()
+let loggedUser: User;
+
+const request = new MovieService();
 //request.listAllMovies().then(resp => console.log(resp))
 
-
-
 const questions = [
-    {
-        type: "input",
-        name: "option",
-        message: "Digite uma opção: \n 1 - Baixar Filmes \n 2 - Login \n 3 - Dar avaliação \n 0 - Sair"
-    }
-]
+  {
+    type: "input",
+    name: "option",
+    message:
+      "Digite uma opção: \n 1 - Baixar Filmes \n 2 - Login \n 3 - Dar avaliação \n 4 - Mostrar filmes com média \n 0 - Sair",
+  },
+];
 
 const chooseMovieQuestions = [
-    {
-        type: "input",
-        name: "option",
-        message: "Qual filme?"
-    }
-]
+  {
+    type: "input",
+    name: "option",
+    message: "Qual filme?",
+  },
+];
+
+const loginQuestions = [
+  {
+    type: "input",
+    name: "option",
+    message: "Qual é o seu id de usuário?",
+  },
+];
 
 const rateQuestions = [
-    {
-        type: "input",
-        name: "option",
-        message: "Qual avaliacao de 0 a 5?"
-    }
-]
+  {
+    type: "input",
+    name: "option",
+    message: "Qual avaliacao de 0 a 5?",
+  },
+];
 
 const possibleAnswers = {
-    DOWNLOAD: '1',
-    LOGIN: '2',
-    RATE_MOVIE: '3',
-    EXIT: '0'
-}
+  DOWNLOAD: "1",
+  LOGIN: "2",
+  RATE_MOVIE: "3",
+  SHOW_MOVIES_WITH_AVERAGE: "4",
+  EXIT: "0",
+};
 
 async function run() {
-    const answers = await inquirer.prompt(questions);
+  const answers = await inquirer.prompt(questions);
 
-    const movieService = new MovieService();
+  const movieService = new MovieService();
 
-    switch(answers.option) {
-        case possibleAnswers.DOWNLOAD:
-                movies = await movieService.listAllMovies();
-                console.log(movies);
-                run()
-            break;
+  switch (answers.option) {
+    case possibleAnswers.DOWNLOAD:
+      movies = await movieService.listAllMovies();
+      console.log(movies);
+      run();
+      break;
 
-        case possibleAnswers.LOGIN:
-            
+    case possibleAnswers.LOGIN:
+      const loginAnswer = await inquirer.prompt(loginQuestions);
+      const userFound = users.find((user) => user.id == parseInt(loginAnswer));
+      console.log(userFound);
+      if (userFound) {
+        loggedUser = userFound;
+        console.log("Usuário logado: " + loggedUser.name);
+      }
+      run();
+      break;
+    case possibleAnswers.RATE_MOVIE:
+      let movieId: string;
+      let rate: string;
 
-        case possibleAnswers.RATE_MOVIE:
-            let movieId: string;
-            let rate: string;
+      const chooseMovieAnswers = await inquirer.prompt(chooseMovieQuestions);
+      movieId = chooseMovieAnswers.option;
 
-            const chooseMovieAnswers = await inquirer.prompt(chooseMovieQuestions);
-            movieId = chooseMovieAnswers.option;
+      const rateAnswers = await inquirer.prompt(rateQuestions);
+      rate = rateAnswers.option;
 
-            const rateAnswers = await inquirer.prompt(rateQuestions);
-            rate = rateAnswers.option;
+      const movieIndex = movies.findIndex(
+        (movie) => movie.id == parseInt(movieId)
+      );
+      movies[movieIndex].ratings.push(parseInt(rate));
+      console.log(movies[movieIndex]);
+      run();
 
-            const movieIndex = movies.findIndex((movie) => movie.id == parseInt(movieId))
-            movies[movieIndex].ratings.push(parseInt(rate))
-            console.log(movies[movieIndex]);
-            run()
-
-        break;
-        case possibleAnswers.EXIT:
-            break;
-    }
+      break;
+    case possibleAnswers.SHOW_MOVIES_WITH_AVERAGE:
+      console.log(addAverageToMoviesArray(movies));
+    case possibleAnswers.EXIT:
+      break;
+  }
 }
 
-run()
-
-
-
+run();
 
 //console.log(addManyMoviesWithIdAndReturnANewUser(user, movies, 1, 2, 4))
 // console.log(filterMoviesByIndicativeRating(movies, user))
-
